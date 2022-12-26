5b47e1b4ee5eae06dcb2e56b5ff98df0a3614546
ab. sabury <sabury2020@gmail.com>
2022-12-26 14:33:51 +0430

    home section completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header';
+import Home from './components/home/home';
 function App() {
   return (
     <>
     <Header />
+      <main className='main'>
+    <Home />
+      </main>
     </>
   );
 }
---
+++ src/components/home/social.jsx
@@ -0,0 +1,27 @@
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.instagram.com/ab.wahed_sabury/" className="home__social-icon" target="_blank">
+        <i className="uil uil-instagram"></i>
+      </a>
+
+      <a href="https://www.facebook.com/abdulwahed.abdulwahed.790" className="home__social-icon" target="_blank">
+        <i className="uil uil-facebook"></i>
+      </a>
+
+      <a href="https://twitter.com/SaburyWahed" className="home__social-icon" target="_blank">
+        <i className="uil uil-twitter"></i>
+      </a>
+
+      <a href="https://www.linkedin.com/in/abdul-wahed-sabury-9134a6241/" className="home__social-icon" target="_blank">
+        <i className="uil uil-linkedin"></i>
+      </a>
+
+      <a href="https://github.com/AbdulWahedSabury" className="home__social-icon" target="_blank">
+        <i className="uil uil-github"></i>
+      </a>
+    </div>
+  );
+}
+ 
+export default Social;
